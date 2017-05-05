201c40818d98b35c3de41c61ab9a62f904bb2a17
M4rq1705 <marcospesante11@gmail.com>
2017-05-05 18:20:29 -0400

    Fixed the image slider on the mobile version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 /*-------------------Image Slider hallery.html -------------------------*/
 var slideIndex = 1;
-var c = 1;
 showDivs(slideIndex);
+
+function startSlider(){
+	var slideIndex = 1;
+	showDivs(slideIndex);
+	plusDivs(0);
+}
 
 function plusDivs(n = 1) {
   showDivs(slideIndex += n);
+  setTimeout(plusDivs, 7500)
 }
 
 function currentDiv(n) {
@@ -12,21 +18,22 @@ function currentDiv(n) {
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  var dots = document.getElementsByClassName("imagePreview");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
-  }
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " w3-opacity-off";
-  if(c){setTimeout(plusDivs, 5000);c+=1;}
+	var i;
+	var x = document.getElementsByClassName("slide");
+	var dots = document.getElementsByClassName("imagePreview");
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+	}
+	x[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " w3-opacity-off";
 }
+
+
 /*-----------------Dropdown menu index.html ----------------------------*/
 function clickableDropdown() {
     var x = document.getElementById("navDropdown");
